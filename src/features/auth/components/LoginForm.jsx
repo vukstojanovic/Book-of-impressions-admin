47db2fc3e3d2a7a0ba7b88c9ce0693ef8997d53a
Stefan Meza <stefanmeza93@gmail.com>
2022-07-06 15:34:38 +0200

    Add complete layout for login page

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -1,41 +1,74 @@
-import { Form, Input, Checkbox, Button } from 'antd'
+import { Row, Card, Form, Input, Checkbox, Button, Typography } from 'antd'
+import { Link } from 'react-router-dom'
+
+const { Title, Paragraph } = Typography
 
 export const LoginForm = () => {
+  const onFinish = ({ email, password, remember }) => {
+    console.log(email, password, 'Remember: ' + remember)
+  }
+
   return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{ remember: true }}
-      wrapperCol={{ span: 8 }}
-    >
-      <Form.Item
-        name="username"
-        rules={[{ required: true, message: 'Please input your Username!' }]}
+    <Card style={{ padding: '50px' }}>
+      <Title level={2}>Welcome to Boi!</Title>
+      <Paragraph>
+        New to Boi! <Link to="/sign-up">Create an account</Link>
+      </Paragraph>
+      <Form
+        layout="vertical"
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        wrapperCol={{ span: 24 }}
+        onFinish={onFinish}
       >
-        <Input placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your Password!' }]}
-      >
-        <Input type="password" placeholder="Password" />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
+        <Form.Item
+          label="E-mail"
+          name="email"
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please enter your E-mail!',
+            },
+          ]}
+        >
+          <Input placeholder="Email" size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please enter your Password!' }]}
+        >
+          <Input type="password" placeholder="Password" size="large" />
+        </Form.Item>
+        <Form.Item>
+          <Row justify="space-between">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Row>
         </Form.Item>
 
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            block={true}
+            size="large"
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
   )
 }
