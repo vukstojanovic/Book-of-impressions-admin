5c12a44de8736df6d71b5fdf300e2a6dc2c75740
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2022-07-06 15:21:40 +0200

    signup/signin layout, PageLayout

---
+++ src/components/layout/AdminLayout.jsx
@@ -0,0 +1,35 @@
+import { Col, Row } from 'antd'
+
+export const AdminLayout = ({ children }) => {
+  return (
+    <Row style={{ minHeight: '100vh' }}>
+      <Col
+        span={12}
+        style={{
+          backgroundColor: 'white',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          src="https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/login-bg.26a96a34.svg"
+          alt="image"
+          width={'100%'}
+          style={{ zIndex: 2, transform: 'translate(5rem)' }}
+        />
+      </Col>
+      <Col
+        span={12}
+        style={{
+          backgroundColor: '#fafafa',
+          display: 'grid',
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+      >
+        {/* Forma koju prosledis */}
+        {children}
+      </Col>
+    </Row>
+  )
+}
---
+++ src/components/layout/PageLayout.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react'
+import { Layout, Menu } from 'antd'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  LayoutFilled,
+  FileTextOutlined,
+  MinusOutlined,
+  StarFilled,
+  TeamOutlined,
+} from '@ant-design/icons'
+import { useTranslation } from 'react-i18next'
+
+const { Header, Sider, Content, Footer } = Layout
+
+export const PageLayout = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const { t } = useTranslation('General')
+
+  return (
+    <Layout>
+      <Header
+        style={{
+          padding: 0,
+          display: 'flex',
+        }}
+      >
+        <div
+          style={{
+            width: '100px',
+            height: '32px',
+            margin: '16px',
+            background: 'rgba(255, 255, 255, 0.3)',
+          }}
+        />
+        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+          className: 'trigger',
+          style: {
+            padding: '0 24px',
+            fontSize: '18px',
+            lineHeight: '64px',
+            cursor: 'pointer',
+            color: 'white',
+            transition: 'color 0.3s',
+          },
+          onClick: () => setCollapsed(!collapsed),
+        })}
+        <div
+          style={{
+            display: 'flex',
+            color: 'white',
+            width: '100%',
+            justifyContent: 'right',
+            margin: '0 10px 0 0',
+            gap: 4,
+          }}
+        >
+          <p>Flag</p>
+          <p>Notification</p>
+          <p>Avatar</p>
+        </div>
+      </Header>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <LayoutFilled />,
+                label: 'Dashboard',
+              },
+              {
+                key: '2',
+                icon: collapsed ? <MinusOutlined /> : null,
+                label: collapsed ? '' : 'Main',
+              },
+              {
+                key: '3',
+                icon: <FileTextOutlined />,
+                label: 'Forms',
+              },
+              {
+                key: '4',
+                icon: <StarFilled />,
+                label: 'Reviews',
+              },
+              {
+                key: '5',
+                icon: <FileTextOutlined />,
+                label: 'Reports',
+              },
+              {
+                key: '6',
+                icon: collapsed ? <MinusOutlined /> : null,
+                label: collapsed ? '' : 'Organization',
+              },
+              {
+                key: '7',
+                icon: <TeamOutlined />,
+                label: 'Users',
+              },
+            ]}
+          />
+        </Sider>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+          }}
+        >
+          <h1>{t('header_title')}</h1>
+          {children}
+        </Content>
+      </Layout>
+      <Footer style={{ textAlign: 'center', backgroundColor: '#b6b0b0' }}>Footer</Footer>
+    </Layout>
+  )
+}
