116e16691dd2d0ea4ea3d0fddf9db190d673df60
vukstojanovic <vuks838@gmail.com>
2022-07-06 15:40:45 +0200

    [implement-signup-page] Add SignupForm

---
+++ src/features/auth/components/SignupForm.jsx
@@ -0,0 +1,72 @@
+import { Button, Form, Input, Typography, Row, Col } from 'antd'
+import { Link } from 'react-router-dom'
+import 'antd/dist/antd.css'
+
+const { Title, Paragraph } = Typography
+
+export const SignupForm = () => {
+  const inputStyle = { padding: '10px 8px' }
+  return (
+    <Row justify="center">
+      <Col xs={{ span: 22 }} md={{ span: 20 }}>
+        <Title level={3}>Welcome to Boi</Title>
+        <Paragraph>
+          Already have an account? <Link to="/sign-in">Sign in</Link>
+        </Paragraph>
+        <Form
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          autoComplete="off"
+          layout="vertical"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input size="large" style={inputStyle} />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email!',
+              },
+            ]}
+          >
+            <Input size="large" style={inputStyle} />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password size="large" style={inputStyle} />
+          </Form.Item>
+
+          <Form.Item>
+            <Button block size="large" type="primary" htmlType="submit">
+              Sign Up
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  )
+}
--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -1,4 +1,5 @@
 import { Navigate } from 'react-router-dom'
+import { SignupForm } from '@/features/auth'
 
 export const publicRoutes = [
   {
@@ -7,7 +8,7 @@ export const publicRoutes = [
   },
   {
     path: '/sign-up',
-    element: 'Sign up page',
+    element: <SignupForm />,
   },
   { path: '*', element: <Navigate to="/sign-in" /> },
 ]
