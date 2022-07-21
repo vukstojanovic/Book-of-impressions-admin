dfddf9d3d6d8033e92338c01869a05d74dbb0892
Stefan Meza <stefanmeza93@gmail.com>
2022-07-21 16:33:03 +0200

    Add changes for layout in create-new-form component

--- src/features/forms/components/CreateNewForm.jsx
+++ src/features/forms/components/CreateNewForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Form, Typography, Input, Select, Button, Card, message } from 'antd'
+import { Row, Col, Form, Typography, Input, Select, Button, Card, message } from 'antd'
 
 const { Option } = Select
 const { Title, Text } = Typography
@@ -45,117 +45,146 @@ export const CreateNewForm = () => {
   return (
     <>
       <Title level={2}>{t('main')}</Title>
-      <Form form={addNewForm} onFinish={handleSubmit} size="large">
-        <Form.Item
-          style={{ width: '300px' }}
-          name="title"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input allowClear={true} placeholder={t('formTitle')} />
-        </Form.Item>
-        <Form.Item
-          style={{ width: '300px' }}
-          name="description"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input allowClear={true} placeholder={t('formDescription')} />
-        </Form.Item>
-        <Form.Item
-          style={{ width: '300px' }}
-          name="type"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Select value={type} onChange={handleSelectChange} placeholder={t('formType')}>
-            <Option value="type-1">{t('type1')}</Option>
-            <Option value="type-2">{t('type2')}</Option>
-          </Select>
-        </Form.Item>
+      <Form form={addNewForm} onFinish={handleSubmit} layout="vertical" size="large">
+        <Row gutter={26}>
+          <Col sm={24} md={12} lg={8}>
+            <Form.Item
+              label={t('formTitle')}
+              name="title"
+              rules={[
+                {
+                  required: true,
+                  message: t('emptyTitle'),
+                },
+              ]}
+            >
+              <Input allowClear={true} />
+            </Form.Item>
+          </Col>
+          <Col sm={24} md={12} lg={8}>
+            <Form.Item
+              label={t('formDescription')}
+              name="description"
+              rules={[
+                {
+                  required: true,
+                  message: t('emptyDescription'),
+                },
+              ]}
+            >
+              <Input allowClear={true} />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={24} md={24} lg={16}>
+            <Form.Item
+              label={t('formType')}
+              name="type"
+              rules={[
+                {
+                  required: true,
+                  message: t('emptyType'),
+                },
+              ]}
+            >
+              <Select value={type} onChange={handleSelectChange}>
+                <Option value="type-1">{t('type1')}</Option>
+                <Option value="type-2">{t('type2')}</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
 
         {type === 'type-1' ? (
-          <Card style={{ margin: '1.75rem 0' }}>
-            <Title level={4}>{t('question')}</Title>
-            <Text>{t('warning')}</Text>
-            <div>
-              <Input
-                placeholder={t('inputQuestion')}
-                size="large"
-                value={question}
-                onChange={(e) => setQuestion(e.target.value)}
-              />
-            </div>
-
-            {typeOneQuestion.map((question, i) => (
-              <div
-                key={i}
-                style={{
-                  marginTop: '10px',
-                  padding: '15px 10px',
-                  borderBottom: '0.5px solid black',
-                }}
+          <Row>
+            <Col sm={24} md={24} lg={16}>
+              <Form.Item
+                label={t('question')}
+                rules={[
+                  {
+                    required: true,
+                  },
+                ]}
               >
-                <Text key={i} style={{ fontSize: '1.25rem' }}>
-                  {question.name}
-                </Text>
-              </div>
-            ))}
+                <Card style={{ margin: '1.75rem 0' }}>
+                  <Text>{t('warning')}</Text>
+                  <div>
+                    <Input
+                      placeholder={t('inputQuestion')}
+                      size="large"
+                      value={question}
+                      onChange={(e) => setQuestion(e.target.value)}
+                    />
+                  </div>
 
-            <Button
-              style={{ display: 'block', marginTop: '15px' }}
-              onClick={handleAddQuestion}
-              disabled={typeOneQuestion.length >= 1}
-            >
-              {t('addQuestion')}
-            </Button>
-          </Card>
+                  {typeOneQuestion.map((question, i) => (
+                    <div
+                      key={i}
+                      style={{
+                        marginTop: '10px',
+                        padding: '15px 10px',
+                        borderBottom: '0.5px solid black',
+                      }}
+                    >
+                      <Text key={i} style={{ fontSize: '1.25rem' }}>
+                        {question.name}
+                      </Text>
+                    </div>
+                  ))}
+
+                  <Button
+                    style={{ display: 'block', marginTop: '15px' }}
+                    onClick={handleAddQuestion}
+                    disabled={typeOneQuestion.length >= 1}
+                  >
+                    {t('addQuestion')}
+                  </Button>
+                </Card>
+              </Form.Item>
+            </Col>
+          </Row>
         ) : (
-          <Card style={{ marginBottom: '1.75rem' }}>
-            <Title level={4}>{t('question')}</Title>
-            <Text>{t('warning2')}</Text>
+          <Row>
+            <Col sm={24} md={24} lg={16}>
+              <Card style={{ marginBottom: '1.75rem' }}>
+                <Title level={4}>{t('question')}</Title>
+                <Text>{t('warning2')}</Text>
 
-            <div>
-              <Input
-                placeholder={t('inputQuestion')}
-                size="large"
-                value={question}
-                onChange={(e) => setQuestion(e.target.value)}
-              />
-            </div>
+                <div>
+                  <Input
+                    placeholder={t('inputQuestion')}
+                    size="large"
+                    value={question}
+                    onChange={(e) => setQuestion(e.target.value)}
+                  />
+                </div>
 
-            {typeTwoQuestion.map((question, i) => (
-              <div
-                key={i}
-                style={{
-                  marginTop: '10px',
-                  padding: '15px 10px',
-                  borderBottom: '0.5px solid black',
-                }}
-              >
-                <Text key={i} style={{ fontSize: '1.25rem' }}>
-                  {question.name}
-                </Text>
-              </div>
-            ))}
+                {typeTwoQuestion.map((question, i) => (
+                  <div
+                    key={i}
+                    style={{
+                      marginTop: '10px',
+                      padding: '15px 10px',
+                      borderBottom: '0.5px solid black',
+                    }}
+                  >
+                    <Text key={i} style={{ fontSize: '1.25rem' }}>
+                      {question.name}
+                    </Text>
+                  </div>
+                ))}
 
-            <Button
-              style={{ display: 'block', marginTop: '15px' }}
-              onClick={handleAddQuestion}
-              disabled={typeTwoQuestion.length >= 3}
-            >
-              {t('addQuestion')}
-            </Button>
-          </Card>
+                <Button
+                  style={{ display: 'block', marginTop: '15px' }}
+                  onClick={handleAddQuestion}
+                  disabled={typeTwoQuestion.length >= 3}
+                >
+                  {t('addQuestion')}
+                </Button>
+              </Card>
+            </Col>
+          </Row>
         )}
 
         <Form.Item>
