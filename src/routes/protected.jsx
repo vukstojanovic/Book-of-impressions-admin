8a009355c9d940b98d624c118a1f2b1b1ab16d59
Danilo Markicevic <danilo@pop-os.localdomain>
2022-07-26 14:19:50 +0200

    profile settings page

--- src/routes/protected.jsx
+++ src/routes/protected.jsx
@@ -5,6 +5,7 @@ import { Users, InviteUser } from '@/features/users'
 import { Reviews } from '@/features/reviews'
 import { Forms, CreateNewForm } from '@/features/forms'
 import { Settings } from '@/features/settings'
+import { ProfileSettings } from '@/features/profileSettings'
 
 const App = () => {
   return (
@@ -33,6 +34,7 @@ export const protectedRoutes = [
       { path: '/reviews', element: <Reviews /> },
       { path: '/reports', element: '<Reports />' },
       { path: '/settings', element: <Settings /> },
+      { path: '/my-profile', element: <ProfileSettings /> },
       { path: '/', element: '<Dashboard />' },
       { path: '*', element: <Navigate to="/" /> },
     ],
