b5e228c15769289c8c9a21cf20ca3b6e800eec19
Danilo Markicevic <danilo@pop-os.localdomain>
2022-08-03 09:51:35 +0200

    modify users and settings forms to be more alike

--- src/features/users/components/InviteUser.jsx
+++ src/features/users/components/InviteUser.jsx
@@ -23,7 +23,7 @@ export const InviteUser = () => {
         form={form}
       >
         <Row span={24} gutter={[45]}>
-          <Col xs={{ span: 24 }}>
+          <Col xs={{ span: 14 }}>
             <Form.Item
               label={t('name')}
               name="name"
@@ -38,48 +38,56 @@ export const InviteUser = () => {
             </Form.Item>
           </Col>
         </Row>
-        <Form.Item
-          label={t('email')}
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: t('empty_warning'),
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={t('roles')}
-          name="role"
-          rules={[
-            {
-              required: true,
-              message: t('empty_warning'),
-            },
-          ]}
-        >
-          <Card>
-            <Radio.Group>
-              <Space direction="vertical">
-                {userRoles.map((role) => (
-                  <Radio value={role} key={role}>
-                    <Typography.Paragraph style={{ textTransform: 'capitalize' }}>
-                      {t(role)}
-                    </Typography.Paragraph>
-                    <Typography.Paragraph style={{ color: 'gray', fontSize: '14px' }}>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quo iste.
-                      Beatae nemo libero aliquid dolore eveniet asperiores pariatur exercitationem
-                      quasi? Possimus, at? Cum consequuntur dolorum soluta nulla, natus velit.
-                      Aliquam amet, dolorum tenetur.
-                    </Typography.Paragraph>
-                  </Radio>
-                ))}
-              </Space>
-            </Radio.Group>
-          </Card>
-        </Form.Item>
+        <Row span={24} gutter={[45]}>
+          <Col xs={{ span: 14 }}>
+            <Form.Item
+              label={t('email')}
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: t('empty_warning'),
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row span={24} gutter={[45]}>
+          <Col xs={{ span: 14 }}>
+            <Form.Item
+              label={t('roles')}
+              name="role"
+              rules={[
+                {
+                  required: true,
+                  message: t('empty_warning'),
+                },
+              ]}
+            >
+              <Card>
+                <Radio.Group>
+                  <Space direction="vertical">
+                    {userRoles.map((role) => (
+                      <Radio value={role} key={role}>
+                        <Typography.Paragraph style={{ textTransform: 'capitalize' }}>
+                          {t(role)}
+                        </Typography.Paragraph>
+                        <Typography.Paragraph style={{ color: 'gray', fontSize: '14px' }}>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quo iste.
+                          Beatae nemo libero aliquid dolore eveniet asperiores pariatur
+                          exercitationem quasi? Possimus, at? Cum consequuntur dolorum soluta nulla,
+                          natus velit. Aliquam amet, dolorum tenetur.
+                        </Typography.Paragraph>
+                      </Radio>
+                    ))}
+                  </Space>
+                </Radio.Group>
+              </Card>
+            </Form.Item>
+          </Col>
+        </Row>
         <Row justify="end" span={24} gutter={[10, 10]}>
           <Col>
             <Button style={{ width: '100%' }} onClick={() => form.resetFields()}>
