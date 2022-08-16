758e505f7ee511a3bcfb41c862dc17b5399081a6
Stefan Meza <stefanmeza93@gmail.com>
2022-08-16 15:48:31 +0200

    Delete all logs and comments

--- src/features/reviews/components/Reviews.jsx
+++ src/features/reviews/components/Reviews.jsx
@@ -74,61 +74,6 @@ export const Reviews = () => {
                       </Paragraph>
                     </div>
                   </div>
-                  {/* <Row
-                    justify="space-between"
-                    wrap={true}
-                    align={rating ? 'middle' : 'top'}
-                    style={{ marginBottom: '20px' }}
-                  >
-                    <Col xs={{ span: 24 }} md={{ span: 24 }} xl={{ span: 8 }}>
-                      <div>
-
-                      </div>
-                      <Text style={{ fontWeight: 'bold' }}>{reviewName}</Text>
-                    </Col>
-                    <Col
-                      xs={{ span: 24 }}
-                      sm={{ span: 24, textAlign: 'left' }}
-                      md={{ span: 8 }}
-                      // style={{
-                      //   textAlign: `${!rating && !ratings && !answer ? 'right' : 'center'}`,
-                      // }}
-                    >
-                      <Text>{dayjs(createdDate).format('DD/MM/YYYY')}</Text>
-                    </Col>
-                    {rating && (
-                      <Col
-                        xs={{ span: 24 }}
-                        sm={{ span: 24 }}
-                        md={{ span: 8 }}
-                        style={{ display: 'flex', justifyContent: 'end',  }}
-                      >
-                        <Rate disabled allowHalf defaultValue={rating} />
-                      </Col>
-                    )}
-                    {ratings && (
-                      <Col
-                        xs={{ span: 24 }}
-                        sm={{ span: 24 }}
-                        md={{ span: 8 }}
-                        style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'end' }}
-                      >
-                        {ratings.map((rating, i) => {
-                          return <Rate key={i} disabled allowHalf defaultValue={rating || 0} />
-                        })}
-                      </Col>
-                    )}
-                    {answer && (
-                      <Col
-                        xs={{ span: 24 }}
-                        sm={{ span: 24 }}
-                        md={{ span: 8 }}
-                        style={{ display: 'flex', justifyContent: 'end' }}
-                      >
-                        <LikeOutlined style={{ fontSize: '24px', color: 'green' }} />
-                      </Col>
-                    )}
-                  </Row> */}
                 </Card>
               </Col>
             )
