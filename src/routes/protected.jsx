4ea7d6d99f8301e39cc98e03e7f4339fe567d188
Danilo Markicevic <danilo.markicevic@quantox.com>
2022-08-19 12:39:04 +0200

    added questions starting values

--- src/routes/protected.jsx
+++ src/routes/protected.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { PageLayout } from '@/components/layout'
 import { Users, InviteUser } from '@/features/users'
 import { Reviews } from '@/features/reviews'
-import { Forms, CreateNewForm, FormPreview } from '@/features/forms'
+import { Forms, CreateNewForm, FormPreview, EditForm } from '@/features/forms'
 import { Settings } from '@/features/settings'
 import { ProfileSettings } from '@/features/profileSettings'
 import { Reports } from '@/features/reports'
@@ -32,6 +32,7 @@ export const protectedRoutes = [
       { path: '/profile', element: '<Profile />' },
       { path: '/forms', element: <Forms /> },
       { path: '/forms/create-new-form', element: <CreateNewForm /> },
+      { path: '/forms/edit', element: <EditForm /> },
       { path: '/forms/:formTitle', element: <FormPreview /> },
       { path: '/reviews', element: <Reviews /> },
       { path: '/reports', element: <Reports /> },
