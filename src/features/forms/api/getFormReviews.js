94a81f2cb89bb1bf037e59d5aa4567c3c22b03a7
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-08-29 19:41:17 +0200

    [single-form-filter] Add searchQuery as dependency

--- src/features/forms/api/getFormReviews.js
+++ src/features/forms/api/getFormReviews.js
@@ -6,6 +6,6 @@ export const getFormReviews = ({ id, searchQuery }) => {
   return axios.get(`/api/wapp/reviews/${id}${searchQuery}`)
 }
 
-export const useGetFormReviews = ({ id, searchQuery }) => {
-  return useQuery(['reviews', id], () => getFormReviews({ id, searchQuery }))
+export const useGetFormReviews = (id, searchQuery) => {
+  return useQuery(['formReviews', searchQuery], () => getFormReviews({ id, searchQuery }))
 }
--- src/features/forms/components/FormPreview.jsx
+++ src/features/forms/components/FormPreview.jsx
@@ -23,87 +23,98 @@ export const FormPreview = () => {
   const id = param.get('id')
 
   const location = useLocation()
-  const searchQuery = location.search
+  const searchQuery = decodeURIComponent(location.search)
   const { data: formData, isLoading: formIsLoading } = useGetForm({
     id,
   })
-  const { data: formReviewData, isLoading: reviewIsLoading } = useGetFormReviews({
-    id,
-    searchQuery,
-  })
+  const { data: formReviewData, isLoading: reviewIsLoading } = useGetFormReviews(id, searchQuery)
   const [qrValue, setQrValue] = useState('asd')
 
   const [QRModalVisible, setQRModalVisible] = useState(false)
   const { Paragraph, Text, Title } = Typography
 
-  if (formIsLoading || reviewIsLoading)
-    return (
-      <Row align="middle" justify="center" style={{ minHeight: '30vh' }}>
-        <Spin size="large" />
-      </Row>
-    )
+  // if (formIsLoading || reviewIsLoading)
+  //   return (
+  //     <Row align="middle" justify="center" style={{ minHeight: '30vh' }}>
+  //       <Spin size="large" />
+  //     </Row>
+  //   )
 
   return (
     <>
-      <QRCodeFormModal
-        formTitle={formData?.title}
-        formId={formData?.id}
-        qrValue={qrValue}
-        setModalVisible={setQRModalVisible}
-        modalVisible={QRModalVisible}
-      />
+      {!formIsLoading ? (
+        <>
+          <QRCodeFormModal
+            formTitle={formData?.title}
+            formId={formData?.id}
+            qrValue={qrValue}
+            setModalVisible={setQRModalVisible}
+            modalVisible={QRModalVisible}
+          />
 
-      <EditButton linkTo={`/forms/edit?id=${id}`} />
+          <EditButton linkTo={`/forms/edit?id=${id}`} />
 
-      <Card style={{ borderRadius: '8px' }}>
-        <Row gutter={16} align="middle" style={{ margin: '0 0 12px 0' }}>
-          <Col>
-            <Title level={3} strong style={{ margin: 0 }}>
-              {formData?.title}
-            </Title>
-          </Col>
-          <Col>
-            <Tag color="green">{formData?.type}</Tag>
-          </Col>
-          <Col style={{ marginLeft: 'auto', fontSize: '22px' }}>
-            <QrcodeOutlined
-              onClick={() => {
-                setQrValue(id)
-                setQRModalVisible(true)
-              }}
-            />
-          </Col>
+          <Card style={{ borderRadius: '8px' }}>
+            <Row gutter={16} align="middle" style={{ margin: '0 0 12px 0' }}>
+              <Col>
+                <Title level={3} strong style={{ margin: 0 }}>
+                  {formData?.title}
+                </Title>
+              </Col>
+              <Col>
+                <Tag color="green">{formData?.type}</Tag>
+              </Col>
+              <Col style={{ marginLeft: 'auto', fontSize: '22px' }}>
+                <QrcodeOutlined
+                  onClick={() => {
+                    setQrValue(id)
+                    setQRModalVisible(true)
+                  }}
+                />
+              </Col>
+            </Row>
+            <Paragraph>
+              {formData?.description.filter((lang) => lang.key === language)[0]?.text}
+            </Paragraph>
+            <Paragraph>{t('questions')}: </Paragraph>
+            <ul>
+              {formData?.questions.map((question) => (
+                <li key={question.id}>
+                  <Text>{question.texts.filter((lang) => lang.key === language)[0]?.text}</Text>
+                </li>
+              ))}
+            </ul>
+          </Card>
+        </>
+      ) : (
+        <Row align="middle" justify="center" style={{ minHeight: '30vh' }}>
+          <Spin size="large" />
         </Row>
-        <Paragraph>
-          {formData?.description.filter((lang) => lang.key === language)[0]?.text}
-        </Paragraph>
-        <Paragraph>{t('questions')}: </Paragraph>
-        <ul>
-          {formData?.questions.map((question) => (
-            <li key={question.id}>
-              <Text>{question.texts.filter((lang) => lang.key === language)[0]?.text}</Text>
-            </li>
-          ))}
-        </ul>
-      </Card>
+      )}
       <Divider />
       <Title level={3} style={{ textAlign: 'left', padding: '0 0 0 24px' }}>
         {t('reviews')}:
       </Title>
-      <Col style={{ padding: '0 24px' }}>
-        <FilterComponent
-          hasName
-          hasEmail
-          hasDate
-          hasAnswer={formData?.type === 'Answer'}
-          hasRating={formData?.type === 'Rating' || formData?.type === 'Ratings'}
-        />
-        <Row style={{ gap: 16 }}>
-          {formReviewData[0].map((review) => {
-            return <ReviewCard key={review.id} {...review} />
-          })}
+      {!reviewIsLoading ? (
+        <Col style={{ padding: '0 24px' }}>
+          <FilterComponent
+            hasName
+            hasEmail
+            hasDate
+            hasAnswer={formData?.type === 'Answer'}
+            hasRating={formData?.type === 'Rating' || formData?.type === 'Ratings'}
+          />
+          <Row style={{ gap: 16 }}>
+            {formReviewData[0].map((review) => {
+              return <ReviewCard key={review.id} {...review} />
+            })}
+          </Row>
+        </Col>
+      ) : (
+        <Row align="middle" justify="center" style={{ minHeight: '30vh' }}>
+          <Spin size="large" />
         </Row>
-      </Col>
+      )}
     </>
   )
 }
