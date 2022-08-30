1840a5d72972f027398bdda779922942573cf4bf
Danilo Markicevic <danilo.markicevic@quantox.com>
2022-08-30 15:43:31 +0200

    Tags

--- src/features/settings/components/Settings.jsx
+++ src/features/settings/components/Settings.jsx
@@ -1,6 +1,6 @@
 import { PlusOutlined } from '@ant-design/icons'
-import { Form, Input, Upload, Tabs, Button, Row, Col, Spin } from 'antd'
-import { useState, useEffect } from 'react'
+import { Form, Input, Upload, Tabs, Button, Row, Col, Spin, Tag, Tooltip } from 'antd'
+import { useRef, useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { useUpdateCompanyInfo } from '../api/postCompanyInfo'
@@ -19,6 +19,14 @@ export function Settings() {
 
   const [selectedLogos, setSelectedLogos] = useState()
   const [buttonDisabled, setButtonDisabled] = useState(true)
+
+  const [tags, setTags] = useState(['Unremovable', 'Tag 2', 'Tag 3'])
+  const [inputVisible, setInputVisible] = useState(false)
+  const [inputValue, setInputValue] = useState('')
+  const [editInputIndex, setEditInputIndex] = useState(-1)
+  const [editInputValue, setEditInputValue] = useState('')
+  const inputRef = useRef(null)
+  const editInputRef = useRef(null)
 
   const {
     user: { role },
@@ -104,7 +112,40 @@ export function Settings() {
       </div>
     </div>
   )
+  const handleClose = (removedTag) => {
+    const newTags = tags.filter((tag) => tag !== removedTag)
+    console.log(newTags)
+    setTags(newTags)
+  }
 
+  const showInput = () => {
+    setInputVisible(true)
+  }
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const handleInputConfirm = () => {
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      setTags([...tags, inputValue])
+    }
+
+    setInputVisible(false)
+    setInputValue('')
+  }
+
+  const handleEditInputChange = (e) => {
+    setEditInputValue(e.target.value)
+  }
+
+  const handleEditInputConfirm = () => {
+    const newTags = [...tags]
+    newTags[editInputIndex] = editInputValue
+    setTags(newTags)
+    setEditInputIndex(-1)
+    setInputValue('')
+  }
   // Set Initial Form Values
   useEffect(() => {
     if (company) {
@@ -116,6 +157,14 @@ export function Settings() {
       })
     }
   }, [company])
+  useEffect(() => {
+    if (inputVisible) {
+      inputRef.current?.focus()
+    }
+  }, [inputVisible])
+  useEffect(() => {
+    editInputRef.current?.focus()
+  }, [inputValue])
 
   if (isLoading)
     return (
@@ -247,6 +296,68 @@ export function Settings() {
             {uploadButton}
           </Upload>
         </Form.Item>
+        <Col style={{ display: 'grid', gap: '10px', marginBottom: '20px' }}>
+          {tags.map((tag, index) => {
+            if (editInputIndex === index) {
+              return (
+                <Input
+                  className={style.tagContent}
+                  ref={editInputRef}
+                  key={tag}
+                  value={editInputValue}
+                  onChange={handleEditInputChange}
+                  onBlur={handleEditInputConfirm}
+                  onPressEnter={handleEditInputConfirm}
+                />
+              )
+            }
+
+            const isLongTag = tag.length > 20
+            const tagElem = (
+              <Tag
+                style={{ padding: '4px 6px' }}
+                key={tag}
+                closable={true}
+                onClose={() => handleClose(tag)}
+              >
+                <span
+                  style={{ fontSize: '1rem' }}
+                  onDoubleClick={(e) => {
+                    if (index !== 0) {
+                      setEditInputIndex(index)
+                      setEditInputValue(tag)
+                      e.preventDefault()
+                    }
+                  }}
+                >
+                  {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                </span>
+              </Tag>
+            )
+            return isLongTag ? (
+              <Tooltip title={tag} key={tag}>
+                {tagElem}
+              </Tooltip>
+            ) : (
+              tagElem
+            )
+          })}
+          {inputVisible && (
+            <Input
+              ref={inputRef}
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              onBlur={handleInputConfirm}
+              onPressEnter={handleInputConfirm}
+            />
+          )}
+          {!inputVisible && (
+            <Tag style={{ fontSize: '1rem', padding: '4px 6px' }} onClick={showInput}>
+              <PlusOutlined /> New Tag
+            </Tag>
+          )}{' '}
+        </Col>
         {role !== 'Manager' ? null : (
           <Form.Item style={{ textAlign: 'right' }}>
             <Button type="primary" htmlType="submit" disabled={buttonDisabled ? true : false}>
