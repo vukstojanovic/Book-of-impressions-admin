9037834f8ea16aa58fa5f102f1f7eddc736eebbe
Stefan Meza <stefanmeza93@gmail.com>
2022-09-06 11:03:48 +0200

    Merged from dev

--- src/features/reports/components/Reports.jsx
+++ src/features/reports/components/Reports.jsx
@@ -22,7 +22,6 @@ export const Reports = () => {
   const [isError, setIsError] = useState(false)
 
   const { data: reports, isLoading } = useReports()
-  console.log(reports, isLoading)
 
   // function for fixing misalignment bug in pdf contents
   function removeTextLayerOffset() {
@@ -107,64 +106,6 @@ export const Reports = () => {
     },
   ]
 
-  const data = [
-    {
-      key: '1',
-      name: 'Boi',
-      createdBy: 'Danilo Markicevic',
-      url: (
-        <a href="/sample.pdf" download>
-          sample.pdf
-        </a>
-      ),
-      createdAt: '12.05.2022',
-    },
-    {
-      key: '2',
-      name: 'Boi2',
-      createdBy: 'Vuk Stojanovic',
-      url: (
-        <a href="/masina_za_ves.pdf" download>
-          masina_za_ves.pdf
-        </a>
-      ),
-      createdAt: '11.06.2021',
-    },
-    {
-      key: '3',
-      name: 'Boi3',
-      createdBy: 'Stefan Meza',
-      url: (
-        <a href="/it_academy.pdf" download>
-          it_academy.pdf
-        </a>
-      ),
-      createdAt: '05.08.2022',
-    },
-    {
-      key: '4',
-      name: 'Travel App',
-      createdBy: 'Vlada Stojanovic',
-      url: (
-        <a href="/pdf_test.pdf" download>
-          pdf_test
-        </a>
-      ),
-      createdAt: '24.06.2021',
-    },
-    {
-      key: '5',
-      name: 'Benefit',
-      createdBy: 'Nikola Markovic',
-      url: (
-        <a href="/" download>
-          no pdf available
-        </a>
-      ),
-      createdAt: '10.11.2022',
-    },
-  ]
-
   if (isLoading) {
     return (
       <>
@@ -191,6 +132,21 @@ export const Reports = () => {
       />
     )
   }
+
+  const data = reports[0].map((report) => {
+    const { createdBy, createdDate: createdAt, id: key, name, url } = report
+    return {
+      key,
+      name,
+      createdBy,
+      url: (
+        <a href={url} download>
+          {name}.pdf
+        </a>
+      ),
+      createdAt,
+    }
+  })
 
   return (
     <>
